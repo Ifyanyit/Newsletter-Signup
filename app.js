9efa0f4b54e131540b02dba0c9d6907fe7108720
Ifyanyit <ngeneifyanyi@gamail.com>
2023-02-28 02:35:18 +0100

    added variable 'port'

--- app.js
+++ app.js
@@ -63,9 +63,10 @@ app.post("/failure", function(req, res){
     res.redirect("/");
 });
 
+const port = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Server running on port 3000.")
+app.listen(port, function(){
+    console.log(`Server running on port ${port}`);
 })
 
 //mailchimp Api
